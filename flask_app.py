ce159fa0be9aac9721667996af58534c2e963d33
AlexFairy <AlexM121687@gmail.com>
2025-04-25 17:31:25 -0600

    Fix configuration name for deployment

--- flask_app.py
+++ flask_app.py
@@ -10,9 +10,12 @@ import logging
 
 #originally, this was app = create_app('DelopmentConfig')
 #app = create_app('ProductionConfig')
+'''
 config_name = 'ProductionConfig'
 app = create_app(config_name)
-
+'''
+config_name = 'production'
+app = create_app(config_name)
 
 logging.basicConfig(level=logging.DEBUG)
 app.logger.info("Flask app initialized with Production configuration.")
